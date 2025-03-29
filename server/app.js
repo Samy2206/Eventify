8983056d665a75baea0ca3b2a3a4dd66ffbfa741
Samy2206 <sanketrajuadhav@gmail.com>
2025-03-30 00:23:06 +0530

    env

--- server/app.js
+++ server/app.js
@@ -17,6 +17,8 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
+
+
 //* connect to MongoDB
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true,
     useUnifiedTopology: true,})
