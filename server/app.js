97b0191f8ba618ece4f0781e9d61b70a10d0829e
Samy2206 <sanketrajuadhav@gmail.com>
2025-04-03 17:40:29 +0530

    errorCorrection

--- server/app.js
+++ server/app.js
@@ -2,23 +2,39 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require('./config/db')
 const router = require('./routes/centralizedRoute')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const initializeApp = require('./config/firebase')
+const states = require('./routes/states')
+const student = require('./routes/student')
+const college = require('./routes/college')
+
 const applyMiddleware = require('./middleware/applyMiddleware')
 
 dotenv.config();
 const app = express();
 
-//* Apply middleware for all the api calls
-applyMiddleware(app)
 
 //* initialize firebase admin SDK
 initializeApp()
 
+//* Apply middleware for all the api calls
+applyMiddleware(app)
+
 //* connect to MongoDB
 connectDB()
 
+
 //* Routes
 app.use('/',router)
+
+// app.use('/user/college', college)
+// app.use('/api', states)
+// app.use('/user/student', student)
+
+
+
+
 
 app.listen(process.env.PORT, (req, res) => {
   console.log(`Server is listening at post :${process.env.PORT}`);
